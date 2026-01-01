62ab140ebccaff6e0b0dcba446448bac22abc909
muhammedemir1011 <muhammedemir1011@gmail.com>
2026-01-01 15:28:16 +0300

    bcryptjs compare fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const sqlite3 = require("sqlite3").verbose();
 const http = require("http");
 const { Server } = require("socket.io");
