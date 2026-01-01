d3197fe946adcb47f0aab2ef4fe3f115c067c3a2
muhammedemir1011 <muhammedemir1011@gmail.com>
2026-01-01 15:00:36 +0300

    ana sayfa route eklendi

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.use(
   session({
     secret: "ogrenci-gizli",
